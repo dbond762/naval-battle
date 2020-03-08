1c06f2a969783b005c6895e593ccca9e963b5e8b
dbond762 <dbond762@gmail.com>
2020-03-08 19:07:36 +0200

    add environment

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dbond762/naval-battle
 
 go 1.13
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	gameHttpDelivery "github.com/dbond762/naval-battle/game/delivery/http"
+
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	env := os.Getenv("env")
+	envFile := fmt.Sprintf("%s.env", env)
+	if err := godotenv.Load(envFile); err != nil {
+		log.Print("Environment file not found")
+	}
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./frontend"))
@@ -13,7 +25,9 @@ func main() {
 
 	gameHttpDelivery.NewGameHandler()
 
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	addr := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+	log.Printf("Server run on %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
